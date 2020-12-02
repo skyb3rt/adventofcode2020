1a2ac6b78e242d82d7124d497f81d5b0df45dd30
skyb3rt <58643029+skyb3rt@users.noreply.github.com>
2020-12-02 20:19:24 +0100

    Add files via upload

--- 1_1.py
+++ 1_1.py
@@ -3,11 +3,11 @@ def read_file():
     with open ("1.txt", 'r' ) as file:
         return [int(x) for x in file]
 
-
 def finn_to(verdi,listen):
     for x in range(0,len(listen)):
-        tall= verdi - listen[x]
-        if tall in listen[x:]:  
-            return (tall*listen[x])
+        tall1= verdi - listen[x]
+        if tall1 in listen[x:]:  
+            tall2=listen[x]
+            return (tall1*tall2)
 
 print(finn_to(2020,read_file()))
