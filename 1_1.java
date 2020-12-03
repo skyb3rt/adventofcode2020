1d39fd1c41ab84a43b7f2e91b2e0e7f1af34b854
skyb3rt <58643029+skyb3rt@users.noreply.github.com>
2020-12-03 19:42:30 +0100

    Add files via upload

---
+++ 1_1.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.io.File;
+
+
+class AdvendtOfCode {
+
+    public static void main(String[] args) throws Exception{
+        System.out.println(finn_to(2020,read_file()));
+        
+    }
+        
+    public static ArrayList<Integer> read_file() throws Exception{
+        ArrayList<Integer> l = new ArrayList<Integer>();
+        File myObj = new File("1.txt");
+        Scanner myReader = new Scanner(myObj);
+        while (myReader.hasNextLine()) {
+            int data = Integer.valueOf(myReader.nextLine());
+            l.add(data);        
+        }
+        myReader.close();
+        return l;
+    }
+
+    public static Integer finn_to(Integer verdi, ArrayList<Integer> l){
+        for (int i : l){
+            int num= verdi-i;
+            if (l.contains(num)){
+                return(num* i); // Display the strng.
+            }
+        }
+        return 0;
+    }
+}
